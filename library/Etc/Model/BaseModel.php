c856f3fc336f0ec2e9cf9624d85c0e2523c1f6d0
Marcone Costa <marcone@barraetc.com.br>
2013-04-21 23:36:30 -0300

    #17 alterações na data de atualização dos models

--- library/Etc/Model/BaseModel.php
+++ library/Etc/Model/BaseModel.php
@@ -26,7 +26,7 @@ class Etc_Model_BaseModel extends Zend_Db_Table_Abstract {
     public function update(array $data, $where) {
         $this->_data = $data;
         $this->_data['alteracao_usuario_id'] = $this->_idUsuario;
-        //$this->_data['alteracao_data'] = @date('Y-m-d');
+        $this->_data['alteracao_data'] = @date('Y-m-d  H:i:s');
         return parent::update($this->_data, $where);
     }   
 

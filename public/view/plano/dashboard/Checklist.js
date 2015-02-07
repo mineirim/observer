feb880fded841222cdad1c65ca409cb2c43d90c3
Marcone Costa <trabalho@barraetc.com.br>
2015-02-07 11:22:38 -0200

    #123

--- public/view/plano/dashboard/Checklist.js
+++ public/view/plano/dashboard/Checklist.js
@@ -12,8 +12,10 @@ Ext.define('ExtZF.view.plano.dashboard.Checklist' ,{
             renderer: function(value, p, record){
                         var instrumentos = Ext.StoreMgr.get('Instrumentos');
                         var instrumento = instrumentos.findRecord('id',value);
-                        var formatedString = Ext.String.format('<div class="topic"><b>{0}:</b><span class="author"> {1}</span></div>',
-                         instrumento.get('singular'), record.get('menu'));
+//                        var formatedString = Ext.String.format('<div class="topic"><b>{0}:</b><span class="author"> {1}</span></div>',
+//                         instrumento.get('singular'), record.get('menu'));
+                        var formatedString = Ext.String.format('<div class="topic"><span class="author"> {0}</span></div>',
+                          record.get('menu'));
                         return formatedString;                
            }
         }
@@ -24,16 +26,22 @@ Ext.define('ExtZF.view.plano.dashboard.Checklist' ,{
 //                '<p><b>Descrição:</b> {descricao}</p>'
 //            ]
 //        }],
+                
+                    
+                
     initComponent: function() {
         var me=this;
         Ext.apply(me, {
             viewConfig: {
-               plugins: [{
-                   pluginId: 'preview',
-                   ptype: 'preview',
-                   bodyField: 'descricao',
-                   expanded: false
-               }]
+//               plugins: [{
+//                   pluginId: 'preview',
+//                   ptype: 'preview',
+//                   bodyField: 'menu',
+//                   expanded: false
+//               }],
+                getRowClass: function(record, index) {                    
+                    return (record.get('id') % 2)?'atrasada':'emtempo';
+                }
            }
         });
         me.callParent(arguments);

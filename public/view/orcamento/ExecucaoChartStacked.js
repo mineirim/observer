f10d2714e22cec2639ddf16f7358162de66e84cd
Marcone Costa <marcone@barraetc.com.br>
2016-03-02 15:18:01 -0300

    #206

---
+++ public/view/orcamento/ExecucaoChartStacked.js
@@ -0,0 +1,56 @@
+Ext.define('ExtZF.view.orcamento.ExecucaoChartStacked', {
+    extend: 'Ext.chart.Chart',
+    alias: 'widget.orcamentoExecucaoChartStacked',
+    width: '100%',
+    animate: true,
+    store: 'orcamento.Execucao',
+    shadow: true,
+    title: 'Programação',
+    legend: {
+        position: 'bottom',
+        boxStrokeWidth: 0,
+        labelFont: '9px Helvetica'
+    },
+    axes: [{
+        type: 'Numeric',
+        position: 'left',
+        fields: ['valor_alocado', 'valor_executado'],
+        label: {
+            renderer: Ext.util.Format.numberRenderer('0,0')
+        },
+        title: '%',
+        grid: true,
+        minimum: 0
+    }, {
+        type: 'Category',
+        position: 'bottom',
+        fields: ['numeracao'],
+    }],
+    series: [{
+        type: 'column',
+        axis: 'bottom',
+        highlight: true,
+        stacked: true,
+
+
+        title: [
+            '% Executado',
+            '% Saldo'
+            ],
+        xField: 'menu',
+
+        yField: [
+            'percentual',
+            'percentual_saldo',
+            ],
+        tips: {
+            trackMouse: true,
+            style: 'background: #FFF',
+            height: 20,
+            renderer: function(storeItem, item) {
+                var browser = item.series.title[Ext.Array.indexOf(item.series.yField, item.yField)];
+                this.setTitle(browser + ' para ' + storeItem.get('menu') + ': ' + storeItem.get(item.yField));
+            }
+        }
+    }]
+});

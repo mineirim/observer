e52f6989cd21cf5df22f88e9bede2f79cfe5bad3
Marcone Costa <marcone@barraetc.com.br>
2016-09-23 15:50:15 -0300

    closes #232

--- application/modules/data/models/DbTable/OperativosHistorico.php
+++ application/modules/data/models/DbTable/OperativosHistorico.php
@@ -1,57 +1,64 @@
 <?php
 
-class Data_Model_DbTable_OperativosHistorico extends Zend_Db_Table_Abstract
-{
+class Data_Model_DbTable_OperativosHistorico extends Zend_Db_Table_Abstract {
 
-    protected $_name = 'operativos_historico';
+	/**
+	 * @var string
+	 */
+	protected $_name = 'operativos_historico';
+	/**
+	 * @var string
+	 */
+	protected $_rowClass = 'Data_Model_DbTable_Row_Operativo';
+	/**
+	 * @var array
+	 */
+	protected $_referenceMap = [
+		'Programacoes' => ['columns' => 'programacao_id',
+			'refTableClass' => 'Data_Model_DbTable_Programacoes',
+			'refColumns' => 'id'],
+		'Operativos' => ['columns' => 'operativo_id',
+			'refTableClass' => 'Data_Model_DbTable_Operativos',
+			'refColumns' => 'id']];
 
-    protected $_referenceMap =  [
-                'Programacoes' =>  [ 'columns' => 'programacao_id', 
-                                                          'refTableClass' => 'Data_Model_DbTable_Programacoes', 
-                                                          'refColumns' => 'id' ],            
-                'Operativos' =>  [ 'columns' => 'operativo_id', 
-                                                          'refTableClass' => 'Data_Model_DbTable_Operativos', 
-                                                          'refColumns' => 'id' ] ];
-   
-    /**
-     * Return one page of order entries
-     *
-     * @param int $page page number
-     * @return Zend_Paginator Zend_Paginator
-     */
-    public function getOnePageOfOrderEntries($params, $where=null) {
+	/**
+	 * Return one page of order entries
+	 *
+	 * @param int $page page number
+	 * @return Zend_Paginator Zend_Paginator
+	 */
+	public function getOnePageOfOrderEntries($params, $where = null) {
 
-        $pageNumber = isset($params['page']) ? $params['page'] : 1;
-        if (empty($pageNumber)) { $pageNumber = 1; }
+		$pageNumber = isset($params['page']) ? $params['page'] : 1;
+		if (empty($pageNumber)) {$pageNumber = 1;}
 
-        $limit = isset($params['limit']) ?$params['limit'] :null;
-        if (empty($limit)) { $limit = null; }
+		$limit = isset($params['limit']) ? $params['limit'] : null;
+		if (empty($limit)) {$limit = null;}
 
-        if (!isset($params['sort'])) { 
-            $sort = 'id';             
-        }else{  
-            $sort = $params['sort'].' '.$params['dir'];
-        }              
-        
-        $query = $this->select();
-        if($where){
-            $query->where($where);
-        }
-        $query->order($sort);
-        $paginator = new Zend_Paginator(
-                new Zend_Paginator_Adapter_DbTableSelect($query)
-        );
-        $paginator->setItemCountPerPage($limit);
-        $paginator->setCurrentPageNumber($pageNumber);
-        
-        $rows = [];
-        foreach ($paginator as $record){
-            $rows[]= $record->toArray();
-        }
-        $page = ['rows' => $rows, 
-                      'total'=> $paginator->getTotalItemCount()];
-        return $page;
-    }
+		if (!isset($params['sort'])) {
+			$sort = 'id';
+		} else {
+			$sort = $params['sort'] . ' ' . $params['dir'];
+		}
+
+		$query = $this->select();
+		if ($where) {
+			$query->where($where);
+		}
+		$query->order($sort);
+		$paginator = new Zend_Paginator(
+			new Zend_Paginator_Adapter_DbTableSelect($query)
+		);
+		$paginator->setItemCountPerPage($limit);
+		$paginator->setCurrentPageNumber($pageNumber);
+
+		$rows = [];
+		foreach ($paginator as $record) {
+			$rows[] = $record->toArray();
+		}
+		$page = ['rows' => $rows,
+			'total' => $paginator->getTotalItemCount()];
+		return $page;
+	}
 
 }
-
--- application/modules/data/models/Despesas.php
+++ application/modules/data/models/Despesas.php
@@ -1,32 +1,45 @@
 <?php
 
-class Data_Model_Despesas
-{
+class Data_Model_Despesas {
 
-    public function getArray($params , $where=null,  $relationships=false){
-        $despesas_table = new Data_Model_DbTable_Despesas();
-        $params['limit']='';
-        $despesas = $despesas_table->getOnePageOfOrderEntries($params , $where);       
-        if(!$relationships)
-            return $despesas;
-        $rows = array();
-        $tableFinanceiro = new Data_Model_DbTable_Financeiro();
-        foreach ($despesas['rows'] as $despesa) {
-            
-            $rowFinanceiro = $tableFinanceiro->fetchRow('id='.$despesa['financeiro_id']) ;
-            $financeiro = $rowFinanceiro  ? $rowFinanceiro->toArray() : array();
-            $row = array(
-                'id'                => $despesa['id'],
-                'descricao'         => $despesa['descricao'],
-                'financeiro_id'    => $despesa['financeiro_id'],
-                'valor'             => $despesa['valor'],
-                'financeiro'      => $financeiro
-            );
+	/**
+	 * @param $params
+	 * @param $where
+	 * @param null $relationships
+	 * @return mixed
+	 */
+	public function getArray($params, $where = null, $relationships = false) {
+		$despesas_table  = new Data_Model_DbTable_Despesas();
+		$tableFinanceiro = new Data_Model_DbTable_Financeiro();
+		$params['limit'] = '';
+		$despesas        = $despesas_table->getOnePageOfOrderEntries($params, $where);
+		$row             = $despesas['rows'][0];
+		$financeiroRow   = $tableFinanceiro->fetchRow('id=' . $row['financeiro_id']);
+		$programacaoRow  = $financeiroRow->findParentRow('Data_Model_DbTable_Programacoes');
+		$identity        = Zend_Auth::getInstance()->getIdentity();
+		if (!$identity->is_su && $programacaoRow->responsavel_usuario_id !== $identity->id) {
+			return;
+		}
+		if (!$relationships) {
+			return $despesas;
+		}
 
-            $rows[] = $row;
-        }
-        $despesas['rows']=$rows;
-        return $despesas;
-    }
+		$rows = [];
+		foreach ($despesas['rows'] as $despesa) {
+
+			$rowFinanceiro = $tableFinanceiro->fetchRow('id=' . $despesa['financeiro_id']);
+			$financeiro    = $rowFinanceiro ? $rowFinanceiro->toArray() : [];
+			$row           = [
+				'id' => $despesa['id'],
+				'descricao' => $despesa['descricao'],
+				'financeiro_id' => $despesa['financeiro_id'],
+				'valor' => $despesa['valor'],
+				'financeiro' => $financeiro,
+			];
+
+			$rows[] = $row;
+		}
+		$despesas['rows'] = $rows;
+		return $despesas;
+	}
 }
-
